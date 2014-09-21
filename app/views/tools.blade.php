8a2586240c9e47f641f91dfa4af9ee883c62bb3a
Chris Bolton <chris.bolton97@gmail.com>
2014-09-21 01:21:11 -0400

    updating path to HDD

--- app/views/tools.blade.php
+++ app/views/tools.blade.php
@@ -11,13 +11,13 @@
 		<div class="list-group-header">
 		    <h3 class="list-group-item-title">M.I.T. Open Courseware</h3>
 		</div>
-		<a href="/home/raspberrypi/hddmount/data/mit1/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
+		<a href="/var/www/seed/hdd/data/mit1/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Introduction to Computer Science and Programming</h4>
   		</a>
-  		<a href="/home/raspberrypi/hddmount/data/mit2/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
+  		<a href="/var/www/seed/hdd/data/mit2/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Introduction to Electrical Engineering and Computer Science I</h4>
   		</a>
-  		<a href="/home/raspberrypi/hddmount/data/mit2/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
+  		<a href="/var/www/seed/hdd/data/mit2/contents/Syllabus/index.htm" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Introduction to Computers and Engineering Problem Solving</h4>
   		</a>
 	</div>
@@ -26,7 +26,7 @@
 		<div class="list-group-header">
 		    <h3 class="list-group-item-title">Wikipedia for Students</h3>
 		</div>
-		<a href="/home/raspberrypi/hddmount/data/wikipedia/index.htm" target="_blank" class="list-group-item">
+		<a href="/hdd/data/wikipedia/index.htm" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Access to the best Wikipedia articles for students</h4>
   		</a>
 	</div>
@@ -35,13 +35,13 @@
 		<div class="list-group-header">
 		    <h3 class="list-group-item-title">Free the Children PDFs</h3>
 		</div>
-		<a href="/home/raspberrypi/hddmount/data/ftc/educators.pdf" target="_blank" class="list-group-item">
+		<a href="var/www/seed/hdd/data/ftc/educators.pdf" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Educator's Guide</h4>
   		</a>
-  		<a href="/home/raspberrypi/hddmount/data/ftc/arg/Elementary.pdf" target="_blank" class="list-group-item">
+  		<a href="seed/hdd/data/ftc/arg/Elementary.pdf" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Agriculture and Food Security, Elementary Lesson Package</h4>
   		</a>
-  		<a href="/home/raspberrypi/hddmount/data/ftc/arg/Secondary.pdf" target="_blank" class="list-group-item">
+  		<a href="/seed/hdd/data/ftc/arg/Secondary.pdf" target="_blank" class="list-group-item">
 		    <h4 class="list-group-item-title">Agriculture and Food Security, Secondary Lesson Package</h4>
   		</a>
 	</div>
